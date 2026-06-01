9ac9ba653a424c36653535071533b1959f0eb0e9
tsunhua <lshare@outlook.com>
2026-06-01 22:49:31 +0800

    feat: add OrigIndicator component with tooltip

---
+++ web/src/components/OrigIndicator.vue
@@ -0,0 +1,12 @@
+<template>
+  <span v-if="orig" class="orig-wrap">
+    <span class="orig-mark" @mouseenter="show = true" @mouseleave="show = false" @click="show = !show">&#9998;</span>
+    <span v-if="show" class="orig-tooltip">&#21476;&#65306;{{ orig }}</span>
+  </span>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+defineProps({ orig: { type: String, default: '' } })
+const show = ref(false)
+</script>
