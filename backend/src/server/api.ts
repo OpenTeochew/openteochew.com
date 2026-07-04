4574b69de9bfd97aa6bd59b6f11bb97378e7478e
tsunhua <lshare@outlook.com>
2026-07-04 17:05:58 +0800

    feat(sugg): add POST /api/v1/suggestions public route

--- backend/src/server/api.ts
+++ backend/src/server/api.ts
@@ -4,6 +4,7 @@ import searchRoutes from './routes/search'
 import entriesRoutes from './routes/entries'
 import sourcesRoutes from './routes/sources'
 import articlesRoutes from './routes/articles'
+import suggestionsRoutes from './routes/suggestions'
 
 const api = new Hono<{ Bindings: CloudflareBindings }>()
 
@@ -11,5 +12,6 @@ api.route('/', searchRoutes)
 api.route('/', entriesRoutes)
 api.route('/', sourcesRoutes)
 api.route('/', articlesRoutes)
+api.route('/', suggestionsRoutes)
 
 export default api
