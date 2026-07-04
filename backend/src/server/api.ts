03298d9709781b360af18b92151f99c5e08e2b1a
tsunhua <lshare@outlook.com>
2026-07-04 17:33:24 +0800

    feat(sugg): add admin routes (login, list, patch, export)

--- backend/src/server/api.ts
+++ backend/src/server/api.ts
@@ -5,6 +5,7 @@ import entriesRoutes from './routes/entries'
 import sourcesRoutes from './routes/sources'
 import articlesRoutes from './routes/articles'
 import suggestionsRoutes from './routes/suggestions'
+import adminRoutes from './routes/admin'
 
 const api = new Hono<{ Bindings: CloudflareBindings }>()
 
@@ -13,5 +14,6 @@ api.route('/', entriesRoutes)
 api.route('/', sourcesRoutes)
 api.route('/', articlesRoutes)
 api.route('/', suggestionsRoutes)
+api.route('/', adminRoutes)
 
 export default api
